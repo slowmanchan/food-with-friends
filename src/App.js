a02df6a8fdc6c58758c094eccd1632af1abdbdc5
Norman Chan <slowmanchan@gmail.com>
2017-07-25 20:15:13 -0400

    completed firebase-react tut

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItem: '',
+      username: '',
+      items: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  removeItem(itemId) {
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        })
+      }
+      this.setState({
+        items: newState
+      })
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,15 +68,36 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-            <form>
-              <input type='text' name='username' placeholder="Name"/>
-              <input type='text' name='currentItem' placeholder="What are you brining?" />
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type='text'
+                name='username'
+                placeholder="Name"
+                onChange={this.handleChange}
+                value={this.state.username}
+                />
+              <input
+                type='text'
+                name='currentItem'
+                placeholder="What are you bringing?"
+                onChange={this.handleChange}
+                value={this.state.currentItem}
+                />
               <button>Add Food</button>
             </form>
           </section>
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
+                {this.state.items.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <p>brought by: {item.user}</p>
+                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
